612a3dcaac2ef9294a1c3fc2fa38dda76416f76e
Scott Moss <willscottmoss@gmail.com>
2017-05-18 13:05:36 -0400

    :recycle: make ready for students

--- src/utilities.js
+++ src/utilities.js
@@ -31,31 +31,3 @@ export const filter = (array, cb) => {
     return results
   }, [])
 }
-
-export const every = (array, cb) => {
-  if (!array.length) {
-    return true
-  }
-
-  let result = false
-  for (let i = 0; i < array.length; i++) {
-    result = Boolean(cb(array[i], i, array))
-    if (!result) {
-      return result
-    }
-  }
-  return result
-}
-
-
-export const flatten = (array, result = []) => {
-  return reduce(array, (mem, item) => {
-    if (Array.isArray(item)) {
-      return flatten(item, mem)
-    }
-    mem.push(item)
-    return mem
-  }, result)
-}
-
-
